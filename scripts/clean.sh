b5d0c34e78ae4a3b07c04edb436a993f08f3f642
Michae Hart <hartmj99@cs.washington.edu>
2020-05-05 02:57:41 -0700

    Update function of clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,7 @@ source $(dirname $0)/print_tools.sh;
 
 info_msg "Removing generated files...";
 
-rm ./build/bundle.js >/dev/null 2>&1;
+rm ./build/main-bundle.js >/dev/null 2>&1;
+rm ./build/test-bundle.js >/dev/null 2>&1;
 
 exit 0;
