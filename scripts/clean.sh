c307fbdcdef84156fc739f838c37983e44ec2801
michael-hart99 <michaelxhart@gmail.com>
2020-10-31 21:19:24 -0700

    Add seperate deployment for dev and prod

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -7,4 +7,10 @@ info_msg "Removing generated files...";
 rm ./build/main-bundle.js >/dev/null 2>&1;
 rm ./build/test-bundle.js >/dev/null 2>&1;
 
+info_msg "Cleaning up template files..." &&
+
+cp ./buildTemplate/blank/ids/* ./dev/ids/ &&
+cp ./buildTemplate/blank/.clasp.json ./build/ &&
+cp ./buildTemplate/blank/projectInfo.ts ./dev/;
+
 exit 0;
---
+++ scripts/deployProd.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source $(dirname $0)/print_tools.sh;
+
+base_dir=$(pwd);
+
+info_msg "Copying template files..." &&
+
+cp ./buildTemplate/prod/ids/* ./dev/ids/ &&
+cp ./buildTemplate/prod/.clasp.json ./build/ &&
+cp ./buildTemplate/prod/projectInfo.ts ./dev/ &&
+
+./scripts/build.sh;
+exit_code=$?;
+
+if [ $exit_code -eq 0 ]
+then
+    info_msg "Deploying..." &&
+
+    cd build &&
+    clasp push &&
+    cd ..;
+    exit_code=$?;
+
+    if [ $exit_code -ne 0 ]
+    then
+        clasp login &&
+        clasp push &&
+        cd ..;
+        exit_code=$?;
+    fi
+fi
+
+info_msg "Cleaning up template files...";
+
+cd $base_dir;
+cp ./buildTemplate/blank/ids/* ./dev/ids/;
+cp ./buildTemplate/blank/.clasp.json ./build/;
+cp ./buildTemplate/blank/projectInfo.ts ./dev/;
+
+exit $exit_code;
