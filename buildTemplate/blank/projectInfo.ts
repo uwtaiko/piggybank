65eb66be080b18f5320e02c4955632d8e0875c86
root <root@Michael-Laptop.localdomain>
2020-10-31 20:15:54 -0700

    Add blank build template

---
+++ buildTemplate/blank/projectInfo.ts
@@ -0,0 +1,6 @@
+export const GROUP_NAME: string = '';
+export const START_QUARTER: string = '';
+export const START_YEAR: string = '';
+export const MEMBER_DUES: string = '';
+export const OFFICER_DUES: string = '';
+export const NUM_ATTNS: string = '';
